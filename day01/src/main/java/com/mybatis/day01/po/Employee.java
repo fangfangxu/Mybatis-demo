8b45cfab3661b82252f8bacee02b5bb4d53bb7af
fangfangxu <472848022@qq.com>
2020-05-01 22:59:13 +0800

    commit
    commit

---
+++ day01/src/main/java/com/mybatis/day01/po/Employee.java
@@ -0,0 +1,22 @@
+package com.mybatis.day01.po;
+
+public class Employee {
+    private String sn;
+    private String name;
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
