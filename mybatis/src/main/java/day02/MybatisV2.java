a1461dffd90d14d10c101fc9b3d4382e0ad22af5
fangfangxu <472848022@qq.com>
2020-05-02 20:48:37 +0800

    MybatisV2
    MybatisV2

--- mybatis/src/main/java/day02/MybatisV2.java
+++ mybatis/src/main/java/day02/MybatisV2.java
@@ -4,14 +4,21 @@ package day02;
 import day01.po.Employee;
 import day01.utils.SimpleTypeRegistry;
 import day02.framework.config.Configuration;
+import day02.framework.config.MappedStatement;
+import day02.framework.sqlsource.BoundSql;
+import day02.framework.sqlsource.ParameterMapping;
+import day02.framework.sqlsource.iface.SqlSource;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.junit.Test;
 
+import javax.sql.DataSource;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Mybatis-V2版本
@@ -21,18 +28,158 @@ import java.util.Map;
 public class MybatisV2 {
 
     //private Properties properties = new Properties();
-
     private Configuration configuration = new Configuration();
+    private String namespace;
 
     /**
      * 加载Xml文件
      */
-    private void loadConfiguration() {
+    private void loadConfiguration() throws Exception {
+        String location = "day02/mybatis-config.xml";
+        InputStream inputStream = getResourceAsStream(location);
+        Document document = createDocument(inputStream);
+        //按照mybatis的语义，对XML内容进行解析
+        parseConfiguration(document.getRootElement());
 
     }
 
+    private void parseConfiguration(Element rootElement) throws Exception {
+        Element envirsElement = rootElement.element("environments");
+        parseEnvironments(envirsElement);
+        Element mappersElement = rootElement.element("mappers");
+        parseMappers(mappersElement);
+    }
+
+    private void parseMappers(Element mappersElement) throws Exception {
+        List<Element> mappers = mappersElement.elements("mapper");
+        for (Element mapperElement : mappers) {
+            parseMapper(mapperElement);
+        }
+    }
+
+    /**
+     * 解析Mapper
+     *
+     * @param mapperElement
+     */
+    private void parseMapper(Element mapperElement) throws Exception {
+        String resource = mapperElement.attributeValue("resource");
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(resource);
+        Document document = createDocument(resourceAsStream);
+        //按照映射文件的语义进行解析
+        parseXmlMapper(document.getRootElement());
+    }
+
+    private void parseXmlMapper(Element rootElement) throws Exception {
+        namespace = rootElement.attributeValue("namespace");
+        List<Element> selectElements = rootElement.elements("select");
+        for (Element selectElement : selectElements) {
+            parseStatementElement(selectElement);
+        }
+
+    }
+
+    /**
+     * MappedStatement
+     *
+     * @param selectElement
+     */
+    private void parseStatementElement(Element selectElement) throws Exception {
+        String statementId = selectElement.attributeValue("id");
+        if (statementId == null || statementId.equals("")) {
+            return;
+        }
+        //一个CRUD标签对应一个MappedStatement对象
+        //一个MappedStatement对象有一个statementId来标识，来保证唯一
+        //statementId=namespace+"."+CRUD标签的id属性
+
+        statementId = namespace + "." + statementId;
+        String statementType = selectElement.attributeValue("statementType");
+
+        String parameterTypeName = selectElement.attributeValue("parameterType");
+        String resultTypeClassName = selectElement.attributeValue("resultType");
+
+        Class<?> parameterTypeClass = resolveType(parameterTypeName);
+        Class<?> resultTypeClass = resolveType(resultTypeClassName);
+
+        //SqlSource的封装过程
+        SqlSource sqlSource = createSqlSource(selectElement);
+        //TO DO 建议使用构建者模式去优化
+        MappedStatement mappedStatement = new MappedStatement(statementId, sqlSource, statementType,
+                parameterTypeClass, resultTypeClass);
+        configuration.addMappedStatements(statementId, mappedStatement);
+    }
+
+    //TO DO
+    private SqlSource createSqlSource(Element selectElement) {
+        return null;
+    }
+
+    private Class<?> resolveType(String type) throws Exception {
+        return Class.forName(type);
+    }
+
+
+    private void parseEnvironments(Element envirsElement) {
+        String defaultId = envirsElement.attributeValue("default");
+        List<Element> environments = envirsElement.elements("environment");
+        for (Element element : environments) {
+            if (element.attributeValue("id").equals(defaultId)) {
+                parseDataSource(element);
+                break;
+
+            }
+        }
+    }
+
+    /**
+     * 解析数据源、放入Configuration中
+     *
+     * @param element
+     */
+    private void parseDataSource(Element element) {
+        Element dataSourceElement = element.element("dataSource");
+        String type = dataSourceElement.attributeValue("type");
+        if (type.equals("DBCP")) {
+            BasicDataSource dataSource = new BasicDataSource();
+            Properties properties = parseProperties(dataSourceElement);
+            dataSource.setDriverClassName(properties.getProperty("driver"));
+            dataSource.setUrl(properties.getProperty("url"));
+            dataSource.setUsername(properties.getProperty("username"));
+            dataSource.setPassword(properties.getProperty("password"));
+            configuration.setDataSource(dataSource);
+        }
+    }
+
+    private Properties parseProperties(Element dataSourceElement) {
+        Properties properties = new Properties();
+        List<Element> propertys = dataSourceElement.elements("property");
+        for (Element element : propertys) {
+            String name = element.attributeValue("name");
+            String value = element.attributeValue("value");
+            properties.put(name, value);
+        }
+        return properties;
+    }
+
+    private Document createDocument(InputStream inputStream) {
+        try {
+            SAXReader saxReader = new SAXReader();
+            Document document = saxReader.read(inputStream);
+            return document;
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+
+    private InputStream getResourceAsStream(String location) {
+        return this.getClass().getClassLoader().getResourceAsStream(location);
+    }
+
     @Test
-    public void test() {
+    public void test() throws Exception {
         //需求：查询用户信息
         loadConfiguration();
         //支持简单类型
@@ -53,57 +200,34 @@ public class MybatisV2 {
         System.out.println(employees2);
     }
 
+
     private <T> List<T> selectUserList(String statementId, Object params) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet rs = null;
         List<T> results = new ArrayList<>();
         try {
-            Class.forName(properties.getProperty("db.driver"));
-            connection = DriverManager.getConnection(properties.getProperty("db.url"),
-                    properties.getProperty("db.username"),
-                    properties.getProperty("db.password"));
-            String sql = properties.getProperty("db.sql." + statementId);
+            connection = getConnection();
+//            String sql = properties.getProperty("db.sql." + statementId);
+            //（1）先要获取MappedStatement
+            MappedStatement mappedStatement = configuration.getMappedStatementById(statementId);
+            //（2）再获取MappedStatement中的SqlSource
+            SqlSource sqlSource = mappedStatement.getSqlSource();
+            //（3）通过SqlSource的API处理，获取BoundSql
+            //TO DO
+            BoundSql boundSql = sqlSource.getBoundSql(params);
+            //（4）通过BoundSql获取到SQL语句
+            String sql = boundSql.getSql();
+            //获取预处理 statement
             preparedStatement = connection.prepareStatement(sql);
-            if (SimpleTypeRegistry.isSimpleType(params.getClass())) {
-                preparedStatement.setObject(1, params);
-            } else if (params instanceof Map) {
-                String param = properties.getProperty("db.sql." + statementId + ".params");
-                String[] paramArray = param.split(",");
 
-                for (int i = 0; i < paramArray.length; i++) {
-                    preparedStatement.setObject(i + 1, ((Map) params).get(paramArray[i]));
-                }
-            } else {
-                String param = properties.getProperty("db.sql." + statementId + ".params");
-                String paramclassname = properties.getProperty("db.sql." + statementId + ".paramclassname");
-                Class<?> paramClassType = Class.forName(paramclassname);
-
-                String[] paramArray = param.split(",");
-                for (int i = 0; i < paramArray.length; i++) {
-                    String columnName = paramArray[i];
-                    Field paramDeclaredField = paramClassType.getDeclaredField(columnName);
-                    paramDeclaredField.setAccessible(true);
-                    Object value = paramDeclaredField.get(params);
-                    preparedStatement.setObject(i + 1, value);
-                }
-            }
+            //参数处理
+            //设置参数
+            setParameters(preparedStatement, params, boundSql);
+            //向数据库发出sql执行查询，查询出结果集
             rs = preparedStatement.executeQuery();
-            String resultclassname = properties.getProperty("db.sql." + statementId + ".resultclassname");
-            Class<?> resultType = Class.forName(resultclassname);
-            while (rs.next()) {
-                Object result = resultType.newInstance();
-                ResultSetMetaData metaData = rs.getMetaData();
-                int columnCount = metaData.getColumnCount();
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);
-                    Field field = resultType.getDeclaredField(columnName);
-                    field.setAccessible(true);
-                    field.set(result, rs.getObject(columnName));
-                }
-                results.add((T) result);
-            }
-
+            //处理结果集
+            handleResultSet(rs, results, mappedStatement);
             return results;
 
         } catch (Exception e) {
@@ -134,6 +258,71 @@ public class MybatisV2 {
             }
         }
         return null;
+    }
+
+    /**
+     * 处理结果集
+     *
+     * @param rs
+     * @param results
+     * @param mappedStatement
+     * @param <T>
+     */
+    private <T> void handleResultSet(ResultSet rs, List<T> results, MappedStatement mappedStatement) throws
+            Exception {
+        Class<?> resultType = mappedStatement.getResultTypeClass();
+        while (rs.next()) {
+            Object result = resultType.newInstance();
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metaData.getColumnName(i);
+                Field field = resultType.getDeclaredField(columnName);
+                field.setAccessible(true);
+                field.set(result, rs.getObject(columnName));
+            }
+            results.add((T) result);
+        }
+
+    }
+
+    /**
+     * 设置参数
+     *
+     * @param preparedStatement
+     * @param params
+     */
+    private void setParameters(PreparedStatement preparedStatement, Object params, BoundSql boundSql) throws
+            Exception {
+
+        if (SimpleTypeRegistry.isSimpleType(params.getClass())) {
+            preparedStatement.setObject(1, params);
+        } else if (params instanceof Map) {
+            Map map = (Map) params;
+            List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+
+            for (int i = 0; i < parameterMappings.size(); i++) {
+                preparedStatement.setObject(i + 1, map.get(parameterMappings.get(i).getName()));
+            }
+        } else {
+            // POJO .....
+        }
+    }
+
+    /**
+     * 通过数据源获取连接
+     *
+     * @return
+     */
+    private Connection getConnection() {
+        DataSource dataSource = configuration.getDataSource();
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 
 }
--- mybatis/src/main/java/day02/framework/config/MappedStatement.java
+++ mybatis/src/main/java/day02/framework/config/MappedStatement.java
@@ -1,11 +1,62 @@
 package day02.framework.config;
 
+import day02.framework.sqlsource.iface.SqlSource;
+
 /**
  * 映射文件中的CRUD片段
  */
 public class MappedStatement {
-  private String state;
+    private String statementId;
+    private SqlSource sqlSource;
+    private String statementType;
+    private Class<?> parameterTypeClass;
+    private Class<?> resultTypeClass;
 
+    public MappedStatement(String statementId, SqlSource sqlSource, String statementType, Class<?> parameterTypeClass, Class<?> resultTypeClass) {
+        this.statementId = statementId;
+        this.sqlSource = sqlSource;
+        this.statementType = statementType;
+        this.parameterTypeClass = parameterTypeClass;
+        this.resultTypeClass = resultTypeClass;
+    }
 
+    public String getStatementId() {
+        return statementId;
+    }
 
+    public void setStatementId(String statementId) {
+        this.statementId = statementId;
+    }
+
+    public SqlSource getSqlSource() {
+        return sqlSource;
+    }
+
+    public void setSqlSource(SqlSource sqlSource) {
+        this.sqlSource = sqlSource;
+    }
+
+    public String getStatementType() {
+        return statementType;
+    }
+
+    public void setStatementType(String statementType) {
+        this.statementType = statementType;
+    }
+
+    public Class<?> getParameterTypeClass() {
+        return parameterTypeClass;
+    }
+
+    public void setParameterTypeClass(Class<?> parameterTypeClass) {
+        this.parameterTypeClass = parameterTypeClass;
+    }
+
+    public Class<?> getResultTypeClass() {
+        return resultTypeClass;
+    }
+
+    public void setResultTypeClass(Class<?> resultTypeClass) {
+        this.resultTypeClass = resultTypeClass;
+    }
 }
